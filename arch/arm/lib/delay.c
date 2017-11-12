34f7ebd167e4c426fa89d394c87a0d171fb70a02
Wolfram Sang <wsa+renesas@sang-engineering.com>
2017-11-12 19:52:24 +0100

    HACK: arch: arm{64}: trace when udelay is called
    Definately not for upstream!
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>

--- arch/arm/lib/delay.c
+++ arch/arm/lib/delay.c
@@ -60,6 +60,7 @@ static void __timer_delay(unsigned long cycles)
 
 	while ((get_cycles() - start) < cycles)
 		cpu_relax();
+	trace_printk("%lu\n", cycles);
 }
 
 static void __timer_const_udelay(unsigned long xloops)
--- drivers/mmc/core/core.h
+++ drivers/mmc/core/core.h
@@ -62,6 +62,7 @@ void mmc_set_initial_state(struct mmc_host *host);
 
 static inline void mmc_delay(unsigned int ms)
 {
+	trace_printk("%d ms\n", ms);
 	if (ms <= 20)
 		usleep_range(ms * 1000, ms * 1250);
 	else
