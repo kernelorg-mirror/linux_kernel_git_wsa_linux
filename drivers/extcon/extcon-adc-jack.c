5f788b44bf45448d9f18ed92337f077cd330c359
Wolfram Sang <wsa+renesas@sang-engineering.com>
2024-12-14 18:07:58 +0100

    power: use boolean values with device_init_wakeup()
    device_init_wakeup() second argument is a bool type. Use proper boolean
    values when calling it to match the type and to produce unambiguous code
    which is easier to understand.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>

--- drivers/extcon/extcon-adc-jack.c
+++ drivers/extcon/extcon-adc-jack.c
@@ -154,7 +154,7 @@ static int adc_jack_probe(struct platform_device *pdev)
 	}
 
 	if (data->wakeup_source)
-		device_init_wakeup(&pdev->dev, 1);
+		device_init_wakeup(&pdev->dev, true);
 
 	adc_jack_handler(&data->handler.work);
 	return 0;
